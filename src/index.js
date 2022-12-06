fcd8897be19181eafce984fb81621aecb211b16d
Phan Minh <pminh.vcbs@vcbs.com>
2022-12-06 17:19:55 +0700

    Reading Endpoint API

--- src/index.js
+++ src/index.js
@@ -17,12 +17,54 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req,res) => {    
+    User.find({ }).then((users) =>{
+        res.status(200).send(users)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/users/:id', (req,res) => {    
+    const _id = req.params.id
+    User.findById(_id).then((user) =>{
+        if(!user) {
+            return res.status(404).send('User does not exist')
+        }
+        res.status(200).send(user)                
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
         res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({ }).then((tasks) =>{
+        res.status(200).send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+
+app.get('/tasks/:id', (req,res) => {    
+    const _id = req.params.id
+    User.findById(_id).then((task) =>{
+        if(!task) {
+            return res.status(404).send('Task does not exist')
+        }
+        res.status(200).send(task)                
+    }).catch((e) => {
+        res.status(500).send(e)
     })
 })
 
